0ed1edd0f92a1125afcf8ab95af32dc80423b6f3
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-11-08 12:14:09 +0600

    After payment gateway, previous code successfully run

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Routes from './src/Navigation/Routes';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <PaperProvider>
+      <Routes />
+    </PaperProvider>
+  );
+}
+
+export default App;
+
+
+// ------> Stripe setup done successfully
 // import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 // import React from 'react'
 // import { StripeProvider } from '@stripe/stripe-react-native'
 // import CheckoutScreen from './stripeNormal/CheckoutScreen'
-
 
 // const App = () => {
 //   return (
@@ -23,44 +41,46 @@
 
 // const styles = StyleSheet.create({})
 
-import { Alert, Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import AllInOneSDKManager from 'paytm_allinone_react-native';
 
-// "c030ee7b9aba490f926c18d2101ac8651667279341156"
+// ---> Paytm environment setup done
+// import { Alert, Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import AllInOneSDKManager from 'paytm_allinone_react-native';
 
-const App = () => {
-  function handlePayment() {
-    AllInOneSDKManager.startTransaction(
-      "da9eb459-38c1-4a90-8c3e-cbd741ddae3c",
-      "iELVJt50414347554560",
-      "470d2a53072e44f6ba56886219c1ea381667885402784",
-      "1.00",
-      `https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=da9eb459-38c1-4a90-8c3e-cbd741ddae3c`,
-      true,
-      false,
-      ''
-    )
-      .then((result) => {
-        console.log(result)
-      })
-      .catch((err) => {
-        console.log(err)
-      });
-  }
-  return (
-    <View>
-      <SafeAreaView>
-        <Text>App</Text>
+// // "c030ee7b9aba490f926c18d2101ac8651667279341156"
 
-        <Button title='paymoneys' color={'blue'} onPress={handlePayment} />
+// const App = () => {
+//   function handlePayment() {
+//     AllInOneSDKManager.startTransaction(
+//       "da9eb459-38c1-4a90-8c3e-cbd741ddae3c",
+//       "iELVJt50414347554560",
+//       "470d2a53072e44f6ba56886219c1ea381667885402784",
+//       "1.00",
+//       `https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=da9eb459-38c1-4a90-8c3e-cbd741ddae3c`,
+//       true,
+//       false,
+//       ''
+//     )
+//       .then((result) => {
+//         console.log(result)
+//       })
+//       .catch((err) => {
+//         console.log(err)
+//       });
+//   }
+//   return (
+//     <View>
+//       <SafeAreaView>
+//         <Text>App</Text>
 
-      </SafeAreaView>
+//         <Button title='paymoneys' color={'blue'} onPress={handlePayment} />
 
-    </View>
-  )
-}
+//       </SafeAreaView>
 
-export default App
+//     </View>
+//   )
+// }
 
-const styles = StyleSheet.create({})
+// export default App
+
+// const styles = StyleSheet.create({})
--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-// /**
-//  * @format
-//  */
-
-// import {register} from '@videosdk.live/react-native-sdk';
-// import {AppRegistry,Platform} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-// import PushNotificationIOS from "react-native-push-notification";
-
-// PushNotificationIOS.configure({
-//     onNotification:function(notification){
-//         console.log('Nofication', notification);
-//     },
-//     permissions:{
-//         alert:true,
-//         badge:true,
-//         sound:true
-//     },
-//     popInitialNotification:true,
-//     requestPermissions:Platform.OS='ios'
-// })
-
-
-
-// register();
-
-// AppRegistry.registerComponent(appName, () => App);
-
-
 /**
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { register } from '@videosdk.live/react-native-sdk';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import PushNotificationIOS from "react-native-push-notification";
+
+PushNotificationIOS.configure({
+    onNotification: function (notification) {
+        console.log('Nofication', notification);
+    },
+    permissions: {
+        alert: true,
+        badge: true,
+        sound: true
+    },
+    popInitialNotification: true,
+    requestPermissions: Platform.OS = 'ios'
+})
+
+
+
+register();
 
 AppRegistry.registerComponent(appName, () => App);
+
+
+
+// ----> for Stripe and PayTM environment setup
+// /**
+//  * @format
+//  */
+
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+// import { name as appName } from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
